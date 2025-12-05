f7f466264b8767325894d8a565a187014a5b1fe7
Pedro Caniato <pedrolucascaniato@gmail.com>
2025-12-05 19:33:33 -0300

    vem que tem

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Esta configuração pode ajudar o Vercel a resolver problemas de compilação do Prisma
-  // forçando o Webpack a não empacotar o Prisma como um módulo externo.
-  webpack: (config) => {
-    config.externals.push({
-      'prisma': 'commonjs prisma',
-      '@prisma/client': 'commonjs @prisma/client',
-    });
-    return config;
-  },
+
 };
 
 module.exports = nextConfig;
